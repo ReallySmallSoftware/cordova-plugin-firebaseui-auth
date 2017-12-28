ff3bd7e208344ecec46478b9ffe03849f5509314
Richard WIndley <richard.windley@gmail.com>
2017-12-28 19:26:02 +0000

    Fix plugin and start to build result

--- src/ios/FirebaseUIAuthPlugin.m
+++ src/ios/FirebaseUIAuthPlugin.m
@@ -17,18 +17,62 @@
 
 - (void)initialise:(CDVInvokedUrlCommand *)command {
     
-    self.providers = [[NSMutableArray alloc] init];
+    NSDictionary *options = [command argumentAtIndex:0 withDefault:@{} andClass:[NSDictionary class]];
+
+    @try {
+        self.providers = [[NSMutableArray<id<FUIAuthProvider>> alloc] init];
     
-    [self.providers addObject:[[FUIGoogleAuth alloc] init]];
-    [self.providers addObject:[[FUIFacebookAuth alloc] init]];
+        NSArray *providers = [options valueForKey:@"providers"];
+        
+        if (providers != nil) {
+            
+            BOOL emailHidden = true;
+            
+            for (NSString *provider in providers) {
+                if ([provider isEqualToString:@"GOOGLE"]) {
+                    [self.providers addObject:[[FUIGoogleAuth alloc] init]];
+                }
+                
+                if ([provider isEqualToString:@"FACEBOOK"]) {
+                    [self.providers addObject:[[FUIFacebookAuth alloc] init]];
+                }
+                
+                if ([provider isEqualToString:@"EMAIL"]) {
+                    emailHidden = false;
+                }
+            }
+            
+            self.authUI.providers = self.providers;
+        
+            [self.authUI setSignInWithEmailHidden:emailHidden];
+        }
+        
+        NSString *tosUrl = [options valueForKey:@"tosUrl"];
+        
+        if (tosUrl != nil) {
+            NSURL *url = [NSURL URLWithString:tosUrl];
+            [self.authUI setTOSURL:url];
+        }
+    }
+    @catch (NSException *exception) {
+        NSLog(@"Initialise error %@", [exception reason]);
+    }
 }
 
 - (void)getToken:(CDVInvokedUrlCommand *)command {
 }
 
 - (void)signIn:(CDVInvokedUrlCommand *)command {
-    UINavigationController *authViewController = [self.authUI authViewController];
-    [self.viewController presentViewController:authViewController animated:YES completion:nil];
+    
+    self.signInCallbackId = command.callbackId;
+    
+    @try {
+        UINavigationController *authViewController = [self.authUI authViewController];
+        [self.viewController presentViewController:authViewController animated:YES completion:nil];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"SignIn error %@", [exception reason]);
+    }
 }
 
 - (BOOL)application:(UIApplication *)app
@@ -44,7 +88,17 @@ options:(NSDictionary *)options {
 
 - (void)authUI:(nonnull FUIAuth *)authUI didSignInWithUser:(nullable FIRUser *)user error:(nullable NSError *)error {
     
+    NSDictionary *result = @{@"email" : [user email],
+                             @"name" : [user displayName],
+                             @"token" : [user refreshToken],
+                             @"id" : [user uid],
+                             @"photoUrl" : [user photoURL]
+                             };
+    
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
+    [pluginResult setKeepCallbackAsBool:YES];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:self.signInCallbackId];
 }
 
 @end
-
